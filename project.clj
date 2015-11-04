b30e4090a7719f6184f1b93f1dc5df41f69311ad
Tim Licata <tim@timothylicata.com>
2015-11-03 19:13:57 -0800

    update the defaults in the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prime-times "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Multiplication tables of prime numbers."
+  :url "http://github.com/tlicata/prime-times"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
