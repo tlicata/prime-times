c70a5e3868d0c7c878697cd6632a9b0fbb2b74ae
Tim Licata <tim@timothylicata.com>
2015-11-03 20:22:18 -0800

    printing empty table, need to do multiplication

--- src/prime_times/core.clj
+++ src/prime_times/core.clj
@@ -1,6 +1,18 @@
-(ns prime-times.core)
+(ns prime-times.core
+  (:require [clojure.math.numeric-tower :refer [exact-integer-sqrt]]
+            [clojure.pprint :refer [print-table]]))
+
+(defn prime? [k]
+  (let [sqrt (first (exact-integer-sqrt k))]
+    (nil? (some #(= 0 (mod k %)) (range 2 (inc sqrt))))))
+
+(defn get-first-n-primes [n]
+  (take n (filter prime? (drop 2 (range)))))
 
 (defn -main
-  "I don't do a whole lot."
+  "Print out a multiplication table of the first 10 prime numbers."
   []
-  (println "Hello, World!"))
+  (let [star "*"
+        primes (get-first-n-primes 10)
+        columns (concat [star] primes)]
+    (print-table columns (map (fn [p] {star p}) primes))))
